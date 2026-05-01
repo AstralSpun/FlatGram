e7c0f4765e5398625bd3eb38b2281d6e4cc96638
relimus <relimus@proton.me>
2026-05-01 14:13:39 +0800

    chore: 配置 tdlib 相关

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,14 @@
+import java.util.Properties
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(localPropertiesFile.inputStream())
+}
+
+val apiId: String = localProperties.getProperty("TELEGRAM_API_ID")?: "0"
+val apiHash: String = localProperties.getProperty("TELEGRAM_API_HASH")?: "\"Unknown\""
+
 plugins {
     alias(libs.plugins.android.application)
 }
@@ -16,6 +27,9 @@ android {
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
+
+        buildConfigField("int", "TELEGRAM_API_ID", apiId)
+        buildConfigField("String", "TELEGRAM_API_HASH", apiHash)
     }
 
     buildTypes {
@@ -29,6 +43,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
 }
 
 dependencies {
@@ -37,4 +55,6 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+
+    implementation(libs.td.lib)
 }
